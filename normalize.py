d780868c323ccd4b791c99e206d9ba12ab076fba
karbo <alexanderkarbo@gmail.com>
2017-06-30 16:23:11 -0400

    initial version

---
+++ normalize.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import sys
+import linecache
+import math
+
+def main():
+    name = sys.argv[1] #read filename from the terminal
+    infile = open(str(name), 'r') #open the data file
+    outfile = open('normalized_'+str(name), 'w') #open a file to write normalized data
+
+    line = infile.readline() #get a line
+    cols = len(line.split()) #find number of columns in file
+
+    for i, l in enumerate(infile): 
+        pass
+    rows = i + 1 #find number of rows in file
+
+    print('\nFile profiled: '+str(cols)+' columns '+str(rows)+' lines\n')
+    
+    value = [[] for i in xrange(cols)] #list of values [column][row]
+    valuesqr = [[] for i in xrange(cols)] #list of squared values [column][row]
+    mean = [[] for i in xrange(cols)] #list of mean of each column [column]
+    meansqr = [[] for i in xrange(cols)] #list of mean of the squared values [column]
+    std = [[] for i in xrange(cols)] #standard deviation of each column [column]
+    col_width = [[] for i in xrange(cols)] #width of each column [column]
+    label = [[] for i in xrange(cols)] #name of each column [column]
+
+    row = linecache.getline(str(name), 1) #get first line
+    for i in range(cols):
+        label[i] = row.split()[i] #read first line for column names
+
+    for j in range(rows):
+        row = linecache.getline(str(name), j+2) #get the line j
+        for i in range(cols):
+            value[i].append(row.split()[i]) #read ith value of j into [i][j]
+        
+    infile.close()
+    print "Values read\n"
+
+    for i in range(cols):
+        mean[i] = sum(float(a) for a in value[i])/rows #calulate mean for each column
+        for j in range(rows):
+            value[i][j] = float(value[i][j]) - mean[i] #subtract the mean from the value
+            valuesqr[i].append(value[i][j] * value[i][j]) #square the new value
+            value[i][j] = str(value[i][j]) #convert value back to a str so it has a length
+        meansqr[i] = sum(float(a) for a in valuesqr[i])/rows #calculate mean of sqrvalues
+        std[i] = math.sqrt(meansqr[i]) #take the sqrt of the mean which is the std
+        col_width[i] = len(max(value[i], key=len))+3 #find the max length of each column
+
+    print "Calculations complete\n"
+
+
+
+#write column means
+    outfile.write ("Mean: ")
+    for i in range(cols):
+        outfile.write("{0:>{1}}".format(str(mean[i]), col_width[i]))
+        outfile.write("  ")
+    outfile.write("\n")
+
+#write column stds
+    outfile.write ("Std:  ")
+    for i in range(cols):
+        outfile.write("{0:>{1}}".format(str(std[i]), col_width[i]))
+        outfile.write("  ")
+    outfile.write("\n\n")
+
+#write column labels
+    outfile.write("      ")
+    for i in range(cols):
+        outfile.write("{0:>{1}}".format(str(label[i]), col_width[i]))
+        outfile.write("  ")
+    outfile.write("\n")
+
+#write column values
+    for j in range(rows): 
+        outfile.write("      ")
+        for i in range(cols):
+            value[i][j] = float(value[i][j]) / std[i]
+            outfile.write("{0:>{1}}".format(str(value[i][j]), col_width[i]))
+            outfile.write("  ")
+        outfile.write("\n")
+    
+    outfile.close()
+    print('Normalized data written to '+'normalized_'+str(name) + '\n')
+
+main()
---
+++ unnormalize.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import sys
+import linecache
+import math
+
+def main():
+    name = sys.argv[1] #read data filename from the terminal
+    name2 = sys.argv[2] #read info filename from terminal
+    num = int(sys.argv[3])#read which mean and std value to use
+    datafile = open(str(name), 'r') #open the data file
+    infofile = open(str(name2), 'r') #open the info file
+    outfile = open('un-normalized_'+str(name), 'w') #open a file to write normalized data
+
+    line = linecache.getline(str(name2), 1)
+    mean = line.split()[num]
+    line = linecache.getline(str(name2), 2)
+    std = line.split()[num]
+    
+    infofile.close()
+
+    line = datafile.readline() #get a line
+    cols = len(line.split()) #find number of columns in file
+
+    for i, l in enumerate(datafile): 
+        pass
+    rows = i + 1 #find number of rows in file
+
+    print('\nFile profiled: '+str(cols)+' columns '+str(rows)+' lines\n')
+    
+    value = [[] for i in xrange(cols)] #list of values [column][row]
+    col_width = [[] for i in xrange(cols)] #width of each column [column]
+    label = [[] for i in xrange(cols)] #name of each column [column]
+
+    row = linecache.getline(str(name), 1) #get first line
+    for i in range(cols):
+        label[i] = str(row.split()[i]) #read first line for column names
+
+    for j in range(rows):
+        row = linecache.getline(str(name), j+2) #get the line j
+        for i in range(cols):
+            value[i].append(row.split()[i]) #read ith value of j into [i][j]
+            value[i][j] = (float(value[i][j]) * float(std)) #multiply by std
+            value[i][j] = value[i][j] + float(mean) #add mean
+            value[i][j] = str(value[i][j]) #convert value to a str so it has a length
+        
+    datafile.close()
+    print "Values un-normalized\n"
+    print "mean: " + mean + "\n"
+    print "std: " + std + "\n"
+
+#write column labels
+    for i in range(cols):
+        col_width[i] = len(max(value[i], key=len))+3 #find the max length of each column
+        outfile.write("{0:>{1}}".format(label[i], col_width[i]))
+        outfile.write("  ")
+    outfile.write("\n")
+
+#write column values
+    for j in range(rows): 
+        for i in range(cols):
+            outfile.write("{0:>{1}}".format(str(value[i][j]), col_width[i]))
+            outfile.write("  ")
+        outfile.write("\n")
+    
+    outfile.close()
+    print('Un-normalized data written to '+'un-normalized_'+str(name) + '\n')
+
+main()
