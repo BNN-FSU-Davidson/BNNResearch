632795c61d7c332fe778fceb9b9742fab43c2be2
Michelle Kuchera <michelle.kuchera@gmail.com>
2018-07-12 16:07:42 -0400

    improving training -- still not good results

--- training/train_MPK.sh
+++ training/train_MPK.sh
@@ -4,21 +4,21 @@
 # date: summer 2017
 # edited: M.P. Kuchera 11 July 2018
 #
-#This code trains BNNs with Radford Neal's FBM software.
-#https://www.cs.toronto.edu/~radford/fbm.software.html
+# breif: This code trains BNNs with Radford Neal's FBM software.
+# https://www.cs.toronto.edu/~radford/fbm.software.html
 #
-#The code assumes that fbm is in the user's path.
-#The file takes 4 arguments, the name of the output file, the datafile, the number
-#of points to use, and the number of cycles to use. The script uses a hybrid montecarlo for a linear
-#regression problem. It also assumes that the user requires 19 inputs and
-#one target, as is the case in the pMSSM.
+# The code assumes that fbm is in the user's path.
+# The file takes 4 arguments, the name of the output file, the datafile, the number
+# of points to use, and the number of cycles to use. The script uses a hybrid montecarlo for a linear
+# regression problem. It also assumes that the user requires 19 inputs and
+# one target, as is the case in the pMSSM.
 #
-#See FBM's documentation for more info
+# See FBM's documentation for more info
 #
-#the next three lines have some saved values for priors:
-#19 20 1 / - 0.05:0.5 0.05:0.5 - x0.05:0.5 - 100
-#19 20 20 1 / - 0.05:1:1.5 0.2:1 - x0.3:1 - 0.2:1 -  x0.1:1:4 - - 10
-#19 10 10 10 1 / - 0.05:1:1.5 0.2:1 - x0.3:1 - 0.2:1 -  x0.1:1:4 - 0.2:1 -  x0.1:1:4 - - 10
+# the next three lines have some saved values for priors:
+# 19 20 1 / - 0.05:0.5 0.05:0.5 - x0.05:0.5 - 100
+# 19 20 20 1 / - 0.05:1:1.5 0.2:1 - x0.3:1 - 0.2:1 -  x0.1:1:4 - - 10
+# 19 10 10 10 1 / - 0.05:1:1.5 0.2:1 - x0.3:1 - 0.2:1 -  x0.1:1:4 - 0.2:1 -  x0.1:1:4 - - 10
 #----------------------------------------------------------------------
 
 name=$1 #first argument is the network file name
